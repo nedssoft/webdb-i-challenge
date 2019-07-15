c84675e43264bd9cd4c643f50c60c2765ec09a02
Orie Chinedu <oriechinedu@gmail.com>
2019-07-15 13:19:56 +0100

    Add a central error handler

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 const express = require('express');
+const { handleError } = require('./helpers')
 
 const db = require('./data/dbConfig.js');
 
 const server = express();
 
 server.use(express.json());
-
+server.get('/', (req, res) => {
+  return  res.send('welcome')
+})
+server.use((err, req, res, next) => {
+  handleError(err, res);
+});
 module.exports = server;
