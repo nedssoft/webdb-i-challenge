b9f71994116c5c82a73f2a1b0c272f4739344b22
Orie Chinedu <oriechinedu@gmail.com>
2019-07-15 14:14:40 +0100

    Create helper functions for interacting with acoounts table

---
+++ data/accounts.js
@@ -0,0 +1,37 @@
+const db = require("./dbConfig");
+
+async function all() {
+  const accounts = await db("accounts");
+  return accounts;
+}
+async function findById(id) {
+  const accounts = await db("accounts")
+    .where({ id })
+    .first();
+  return accounts;
+}
+
+async function createNewAccount(account) {
+  const ids = await db("accounts").insert(account);
+  return ids[0];
+}
+
+async function update(id, account) {
+  const updated = await db("accounts")
+    .where({ id })
+    .update(account);
+}
+
+async function remove(id) {
+  const updated = await db("accounts")
+    .where({ id })
+    .del();
+}
+
+module.exports = {
+  all,
+  createNewAccount,
+  update,
+  remove,
+  findById
+}
